7c59b4eb61b619519014f8b8b2d5f64355219153
Phi Anh Kiet <kiet.pa06@gmail.com>
2021-11-05 13:09:06 +0700

    no message

--- zefyr-0.10.0/lib/src/rendering/text_line.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/rendering.dart';
-
-/// Base class for rendering a line of text in Zefyr editor.
-abstract class RenderTextLine extends RenderBox
-    with RelayoutWhenSystemFontsChangeMixin {}
